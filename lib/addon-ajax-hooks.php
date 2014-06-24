ec781ea03c5fbc41db96d727eaed9a7442ab9aa6
Lew Ayotte <lew@ithemes.com>
2014-06-24 15:09:26 -0500

    Escaping HTML for VAT calculator

--- lib/addon-ajax-hooks.php
+++ lib/addon-ajax-hooks.php
@@ -76,8 +76,8 @@ function it_exchange_easy_eu_value_added_taxes_add_edit_product_vat_calculator()
 	}
 	
 	$return = array(
-		'pre-vat' => it_exchange_format_price( $results['pre-vat'] ),
-		'post-vat' => it_exchange_format_price( $results['post-vat'] ),
+		'pre-vat' => esc_html( it_exchange_format_price( $results['pre-vat'] ) ),
+		'post-vat' => esc_html( it_exchange_format_price( $results['post-vat'] ) ),
 	);
 		
 	die( json_encode( $return ) );
