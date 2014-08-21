905bc0d7cfac7c296e77cf3cd550d2fda5a59315
Lew Ayotte <lew@ithemes.com>
2014-08-21 14:17:01 -0500

    Changing nonce in VAT Session save

--- init.php
+++ init.php
@@ -23,6 +23,7 @@ include( 'lib/addon-settings.php' );
  * We decided to place all AJAX hooked functions into this file, just for ease of use
 */
 include( 'lib/addon-ajax-hooks.php' );
+
 /**
  * The following file contains utility functions specific to our customer pricing add-on
  * If you're building your own addon, it's likely that you will
--- lib/addon-ajax-hooks.php
+++ lib/addon-ajax-hooks.php
@@ -94,37 +94,31 @@ function it_exchange_easy_eu_value_added_taxes_save_vat_number() {
 	$errors = array();
 		
 	if ( ! empty( $_POST ) ) {
-		
-		if ( wp_verify_nonce( $_POST['it-exchange-easy-eu-value-added-taxes-add-edit-vat-number-nonce'], 'it-exchange-easy-eu-value-added-taxes-add-edit-vat-number' ) ) {
-						
-	        if ( empty( $_POST['eu-vat-country'] ) ) {
-	            $errors[] = __( 'You must select a valid EU Country.', 'LION' );
-	        } else {
-	            $vat_country = $_POST['eu-vat-country'];
-	        }
-	        
-	        if ( empty( $_POST['eu-vat-number'] ) ) {
-	            $errors[] = __( 'You must enter a valid VAT number.', 'LION' );
-	        } else {
-	            $vat_number = $_POST['eu-vat-number'];
-	        }
-	        		        
-	        if ( empty( $errors ) ) {
-	        	$return = it_exchange_easy_eu_value_added_taxes_addon_verify_vat( $vat_country, $vat_number );
-	        	
-				if ( true === $return ) {
-					$tax_session = it_exchange_get_session_data( 'addon_easy_eu_value_added_taxes' );
-					$tax_session['vat_country'] = $vat_country;
-					$tax_session['vat_number'] = $vat_number;
-					it_exchange_update_session_data( 'addon_easy_eu_value_added_taxes', $tax_session );
-					wp_send_json_success();
-				} else
-					$errors[] = __( 'Unable to verify VAT Number, please try again', 'LION' );
-								}		
-		} else {
-			
-			$errors[] = __( 'Unable to verify security token, please try again', 'LION' );
-			
+					
+        if ( empty( $_POST['eu-vat-country'] ) ) {
+            $errors[] = __( 'You must select a valid EU Country.', 'LION' );
+        } else {
+            $vat_country = $_POST['eu-vat-country'];
+        }
+        
+        if ( empty( $_POST['eu-vat-number'] ) ) {
+            $errors[] = __( 'You must enter a valid VAT number.', 'LION' );
+        } else {
+            $vat_number = $_POST['eu-vat-number'];
+        }
+        		        
+        if ( empty( $errors ) ) {
+        	$return = it_exchange_easy_eu_value_added_taxes_addon_verify_vat( $vat_country, $vat_number );
+        	
+			if ( true === $return ) {
+				$tax_session = it_exchange_get_session_data( 'addon_easy_eu_value_added_taxes' );
+				$tax_session['vat_country'] = $vat_country;
+				$tax_session['vat_number'] = $vat_number;
+				it_exchange_update_session_data( 'addon_easy_eu_value_added_taxes', $tax_session );
+				wp_send_json_success();
+			} else {
+				$errors[] = __( 'Unable to verify VAT Number, please try again', 'LION' );
+			}		
 		}
 		
 	}
