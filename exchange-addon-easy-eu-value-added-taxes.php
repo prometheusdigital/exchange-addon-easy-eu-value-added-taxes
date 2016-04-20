3069d5a0fbf4b6838bc45a971ffcb3e63f8b5e3e
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-04-20 12:04:09 -0400

    Tagging 1.7.2

--- exchange-addon-easy-eu-value-added-taxes.php
+++ exchange-addon-easy-eu-value-added-taxes.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Easy EU Value Added Taxes
- * Version: 1.7.1
+ * Version: 1.7.2
  * Description: Adds Easy EU Value Added Taxes to iThemes Exchange.
  * Plugin URI: http://ithemes.com/exchange/easy-eu-value-added-taxes/
  * Author: iThemes
